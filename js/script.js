56187d84ea4302ee9d95b43af5d8c2fab6d92632
tomasdarienko <darienkotomas@gmail.com>
2023-11-23 19:18:52 +0100

    finito email

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 let email = prompt('inserisci la tua email')
-
+let risultato = console.log("")
 
 let database = ["ba@gmail", "ma@gmail", "ta@gmail", "fa@gmail"];
 
 for (let i = 0; i < database.length; i++) {
      
           if (database[i] == email) {
-               alert('ti sei loggato')
+               risultato = console.log("ti sei loggato")
           }
+          else if (database[i] !== email) {
+               risultato = console.log("non ti sei loggato")
+          }
+      
      }
 
 
-   
 
 
